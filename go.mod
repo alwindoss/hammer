7efda28c6f9bfbef651c9f59edec19baa0a28acf
Alwin Doss <alwindoss84@gmail.com>
2020-10-02 20:44:18 +0530

    Init Project
    Signed-off-by: Alwin Doss <alwindoss84@gmail.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alwindoss/hammer
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+)
+
+func main() {
+	engine := html.New("./templates", ".html")
+	// Reload the templates on each render, good for development
+	engine.Reload(true) // Optional. Default: false
+
+	// Debug will print each template that is parsed, good for debugging
+	engine.Debug(true) // Optional. Default: false
+
+	// Layout defines the variable name that is used to yield templates within layouts
+	engine.Layout("embed") // Optional. Default: "embed"
+	// Delims sets the action delimiters to the specified strings
+	engine.Delims("{{", "}}") // Optional. Default: engine delimiters
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	// app.Static("/public", "./public")
+	app.Static("/public", "./public")
+
+	// To render a template, you can call the ctx.Render function
+	// Render(tmpl string, values interface{}, layout ...string)
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"Title": "Home",
+		}, "layouts/default")
+	})
+
+	// Render with layout example
+	app.Get("/about", func(c *fiber.Ctx) error {
+		return c.Render("about", fiber.Map{
+			"Title": "About",
+		}, "layouts/default")
+	})
+	app.Get("/loginpage", func(c *fiber.Ctx) error {
+		return c.Render("login", fiber.Map{
+			"Title": "Login",
+		}, "layouts/default")
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
