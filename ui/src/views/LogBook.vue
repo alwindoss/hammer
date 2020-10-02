6c2ff89d821b3f81cfa9e092672ff1324fad32ce
Alwin Doss <alwindoss84@gmail.com>
2020-10-02 23:32:55 +0530

    Edited the data table to reflect logbook
    Signed-off-by: Alwin Doss <alwindoss84@gmail.com>

--- ui/src/views/LogBook.vue
+++ ui/src/views/LogBook.vue
@@ -1,8 +1,8 @@
 <template>
   <v-data-table
     :headers="headers"
-    :items="desserts"
-    sort-by="calories"
+    :items="logBookEntries"
+    sort-by="time"
     class="elevation-1"
   >
     <template v-slot:top>
@@ -26,32 +26,56 @@
                 <v-row>
                   <v-col cols="12" sm="6" md="4">
                     <v-text-field
+                      v-model="editedItem.time"
+                      label="Time"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.callSign"
+                      label="Call Sign"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.band"
+                      label="Band"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.mode"
+                      label="Mode"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.power"
+                      label="Power"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.signalOut"
+                      label="Signal Out"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.signalIn"
+                      label="Signal In"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
                       v-model="editedItem.name"
-                      label="Dessert name"
+                      label="Name"
                     ></v-text-field>
                   </v-col>
                   <v-col cols="12" sm="6" md="4">
                     <v-text-field
-                      v-model="editedItem.calories"
-                      label="Calories"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.fat"
-                      label="Fat (g)"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.carbs"
-                      label="Carbs (g)"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.protein"
-                      label="Protein (g)"
+                      v-model="editedItem.notes"
+                      label="Notes"
                     ></v-text-field>
                   </v-col>
                 </v-row>
@@ -100,33 +124,47 @@ export default {
     dialog: false,
     dialogDelete: false,
     headers: [
-      {
-        text: "Dessert (100g serving)",
-        align: "start",
-        sortable: false,
-        value: "name",
-      },
-      { text: "Calories", value: "calories" },
-      { text: "Fat (g)", value: "fat" },
-      { text: "Carbs (g)", value: "carbs" },
-      { text: "Protein (g)", value: "protein" },
-      { text: "Actions", value: "actions", sortable: false },
+    //   {
+    //     text: "Dessert (100g serving)",
+    //     align: "start",
+    //     sortable: false,
+    //     value: "name",
+    //   },
+      { text: "Time", value: "time" },
+      { text: "Call Sign", value: "callSign" },
+      { text: "Band", value: "band" },
+      { text: "Mode", value: "mode" },
+    //   { text: "Power", value: "actions", sortable: false },
+      { text: "Power", value: "power"},
+      { text: "Signal Out", value: "signalOut"},
+      { text: "Signal In", value: "signalIn"},
+      { text: "Name", value: "name"},
+      { text: "Notes", value: "notes"},
+      { text: "Actions", value: "actions"},
     ],
-    desserts: [],
+    logBookEntries: [],
     editedIndex: -1,
     editedItem: {
-      name: "",
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
+      time: "",
+      callSign: 0,
+      band: 0,
+      mode: 0,
+      power: 0,
+      signalOut: 0,
+      signalIn: 0,
+      name: 0,
+      notes: 0,
     },
     defaultItem: {
-      name: "",
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
+      time: "",
+      callSign: 0,
+      band: 0,
+      mode: 0,
+      power: 0,
+      signalOut: 0,
+      signalIn: 0,
+      name: 0,
+      notes: 0,
     },
   }),
 
@@ -151,76 +189,28 @@ export default {
 
   methods: {
     initialize() {
-      this.desserts = [
+      this.logBookEntries = [
         {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
+          time: "Friday, 3 October 2019 (IST)",
+          callSign: "vu2fi",
+          band: 6.0,
+          model: 24,
+          power: 4.0,
+          signalOut: 4.0,
+          signalIn: 4.0,
+          name: 4.0,
+          notes: 4.0,
         },
         {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
+          time: "Friday, 2 October 2020 (IST)",
+          callSign: "vu2XYZ",
+          band: 6.0,
+          model: 24,
+          power: 4.0,
+          signalOut: 4.0,
+          signalIn: 4.0,
+          name: 4.0,
+          notes: 4.0,
         },
       ];
     },
@@ -238,7 +228,7 @@ export default {
     },
 
     deleteItemConfirm() {
-      this.desserts.splice(this.editedIndex, 1);
+      this.logBookEntries.splice(this.editedIndex, 1);
       this.closeDelete();
     },
 
@@ -260,9 +250,9 @@ export default {
 
     save() {
       if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+        Object.assign(this.logBookEntries[this.editedIndex], this.editedItem);
       } else {
-        this.desserts.push(this.editedItem);
+        this.logBookEntries.push(this.editedItem);
       }
       this.close();
     },
