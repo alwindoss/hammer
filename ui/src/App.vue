bc6734f636b9d6b6df798afb5c932fde439dfa94
Alwin Doss <alwindoss84@gmail.com>
2020-10-02 22:10:12 +0530

    Fixed the navigation
    Signed-off-by: Alwin Doss <alwindoss84@gmail.com>

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,62 +1,50 @@
 <template>
   <v-app id="inspire">
-    <Nav v-bind:navItems=navItems />
-    <v-app-bar app clipped-left>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item link to="/">
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/about">
+          <v-list-item-action>
+            <v-icon>mdi-email</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="indigo" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Hammer</v-toolbar-title>
+      <v-toolbar-title>Viovoi</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
-      <!-- <v-container class="fill-height" fluid> -->
       <v-container fluid>
         <router-view></router-view>
       </v-container>
     </v-main>
-
-    <v-footer app>
-      <span>&copy; Alwin Doss, {{ new Date().getFullYear() }}</span>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-import Nav from '@/components/navigation/Nav.vue';
 export default {
-  components: {
-    Nav,
-  },
   props: {
     source: String,
   },
 
   data: () => ({
     drawer: null,
-    navItems: [
-      {
-        to: "/",
-        title: "Home",
-        icon: "mdi-view-dashboard"
-      },
-      {
-        to: "/lesson",
-        title: "Lessons",
-        icon: "mdi-bandcamp"
-      },
-      {
-        to: "/lessonplan",
-        title: "Lesson Plans",
-        icon: "mdi-ballot"
-      },
-      {
-        to: "/student",
-        title: "Students",
-        icon: "mdi-account-group"
-      },
-    ]
   }),
-
-  created() {
-    this.$vuetify.theme.dark = false;
-  },
 };
 </script>
